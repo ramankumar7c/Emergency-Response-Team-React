75dbaf46fccb865ec0e7144c637ee3547ec01a11
Raman Kumar <ramankumar7c@gmail.com>
2024-07-19 21:59:49 +0530

    Updates

---
+++ precache-manifest.0bce538b98472429aa835491e46a2c1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11089817904492af0fed1e17259731c",
+    "url": "/Emergency-Response-Team-React/index.html"
+  },
+  {
+    "revision": "792bd1f62e77cc38cbf5",
+    "url": "/Emergency-Response-Team-React/static/css/main.431b9573.chunk.css"
+  },
+  {
+    "revision": "864bcd2978ceb370efcb",
+    "url": "/Emergency-Response-Team-React/static/js/2.8890a515.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Emergency-Response-Team-React/static/js/2.8890a515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a57361f1857c64a9dc",
+    "url": "/Emergency-Response-Team-React/static/js/3.0b5be51d.chunk.js"
+  },
+  {
+    "revision": "792bd1f62e77cc38cbf5",
+    "url": "/Emergency-Response-Team-React/static/js/main.a86f28cb.chunk.js"
+  },
+  {
+    "revision": "487addaf5f448211013a",
+    "url": "/Emergency-Response-Team-React/static/js/runtime-main.58322168.js"
+  }
+]);
